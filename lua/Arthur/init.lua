3d369d0ad6a266d6ef3f73af9d2955663577ced3
Arlantacious <aarlantaa@gmail.com>
2024-02-25 17:02:12 -0500

    remove requires

--- lua/Arthur/init.lua
+++ lua/Arthur/init.lua
@@ -2,14 +2,5 @@ require("Arthur.core.lazy")
 require("Arthur.core.keymaps")
 require("Arthur.core.options")
 
-require("Arthur.plugins.colorscheme")
-require("Arthur.plugins.autopairs")
-require("Arthur.plugins.telescope")
-require("Arthur.plugins.harpoon")
-require("Arthur.plugins.fugitive")
-require("Arthur.plugins.treesitter")
-require("Arthur.plugins.lsp")
-require("Arthur.plugins.cmp")
-
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
