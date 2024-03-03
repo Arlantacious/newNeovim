a1735d7b033bacd8bbc91c9d19faffa5ac638aff
Arlantacious <aarlantaa@gmail.com>
2024-03-03 15:52:47 -0500

    migrate config

---
+++ lua/Arthur/init.lua
@@ -0,0 +1,6 @@
+require("Arthur.core.lazy")
+require("Arthur.core.keymaps")
+require("Arthur.core.options")
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
---
+++ lua/Arthur/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+
+			ensure_installed = { "vimdoc", "lua", "python", "java", "c", "cpp", "go", "asm"},
+
+			sync_install = false,
+			auto_install = true,
+			highlight  = {
+				enable = true
+			}
+		})
+	end
+}
